0814f9cb8276e436e0f8ca499680c6fea62f176e
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2020-10-02 20:47:45 +0300

    ''

--- js/game.js
+++ js/game.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const fireballSize = 22;
+
+const getFireballSpeed = function (isMovingLeft) {
+  return isMovingLeft ? 2 : 5;
+};
+
+const wizardSpeed = 3;
+
+const getWizardHeight = function () {
+  return 1.337 * wizardWidth;
+};
+
+const wizardWidth = 70;
+
+const getWizardX = function (gameFieldWidth) {
+  return (gameFieldWidth - wizardWidth) / 2;
+};
+
+const getWizardY = function (gameFieldHeight) {
+  return gameFieldHeight / 3;
+};
+
+
 window.GameConstants = {
   Fireball: {
     size: fireballSize || 24,
